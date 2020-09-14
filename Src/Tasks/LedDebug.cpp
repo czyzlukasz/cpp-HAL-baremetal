2dc9e40b815d81102ff3df9ded206aaef0e3ad5e
Łukasz Czyż <krawacik3@gmail.com>
2020-09-14 21:03:56 +0200

    Added sample LED flashing task

--- Src/Tasks/LedDebug.cpp
+++ Src/Tasks/LedDebug.cpp
@@ -1,13 +1,17 @@
 #include <LedDebug.hpp>
+#include <Hardware.hpp>
+
 
 LedDebug::LedDebug() : Task({}, 500, 0), i(){
 
 }
 
 void LedDebug::initialize() {
-    i = 1234567;
+    Hardware::configureClocks();
+    Hardware::enableGpio(GPIOC, GPIO_PIN_13, Gpio::Direction::Output);
 }
 
 void LedDebug::run() {
+    Hardware::toggle(GPIOC, GPIO_PIN_13);
     ++i;
 }
