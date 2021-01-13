bc065d78761f06a71deeadfd3a509dd84b001970
Łukasz Czyż <krawacik3@gmail.com>
2021-01-13 15:08:47 +0100

    Reduced stack size as tasks are created on heap

--- Src/Tasks/TaskManager.cpp
+++ Src/Tasks/TaskManager.cpp
@@ -57,7 +57,7 @@ void TaskManager::startTasks() {
     // Create all task by calling executeTask(). This is done by passing task pointer to taskEntryPoint.
     // TODO: Add stack size as a parameter
     for(const auto& task : tasks){
-        xTaskCreate(taskEntryPoint, task->getName(), 512, task.get(), task->getPriority(), nullptr);
+        xTaskCreate(taskEntryPoint, task->getName(), 64, task.get(), task->getPriority(), nullptr);
     }
 }
 
